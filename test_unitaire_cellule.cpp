f279a4ec82466eba24aa987031e389b64735663c
AshT <yanis.bouzjallikht@viacesi.fr>
2025-01-20 14:21:35 +0100

    Changement complet --> workshop frac

--- test_unitaire_cellule.cpp
+++ test_unitaire_cellule.cpp
@@ -4,31 +4,55 @@
 
 using namespace std;
 
-// Constructeur par défaut
 void testConstructeur() {
-    Cellule c; 
-    assert(c.getEtat() == 0); // Si getEtat == 0 alors --> OK
-    cout << "Le constructeur est fonctionnel" << endl;
+    Cellule c;
+    if (c.getEtat() == 0) {
+        cout << "[OK] Default constructor works" << endl;
+    } else {
+        cout << "[FAILURE] Default constructor doesn't work" << endl;
+    }
 }
-// Utilisation du constructeur paramétré
+
 void testConstructeurP() {
-    Cellule c(1); 
-    assert(c.getEtat() == 1); // Si getEtat == 1 alors --> OK
-    cout << "Le constructeur paramétré est fonctionnel" << endl;
+    Cellule c(1);
+    if (c.getEtat() == 1) {
+        cout << "[OK] Constructor with parameters works" << endl;
+    } else {
+        cout << "[FAILURE] Constructor with parameters doesn't work" << endl;
+    }
 }
-// Utilisation du constructeur par défaut
+
 void testSetEtat() {
-    Cellule c; 
-    c.setEtat(1); // on définit l'état de la cellule en 1
-    assert(c.getEtat() == 1); // le getEtat va checker si l'état est en 1, si oui, alors --> OK
-    std::cout << "Le setEtat a correctement été modifié" << std::endl;
+    Cellule c;
+    c.setEtat(1);
+    if (c.getEtat() == 1) {
+        cout << "[OK] Le setEtat fonctionne" << endl;
+    } else {
+        cout << "[FAILURE] Le setEtat ne fonctionne pas" << endl;
+    }
+}
+
+void testOperatorEgal() {
+    Cellule c1(1);
+    Cellule c2(1);
+    Cellule c3(0);
+    if (c1 == c2) {
+        cout << "[OK] l'opérateur d'égalité fonctionne pour les cellules" << endl;
+    } else {
+        cout << "[FAILURE] l'opérateur d'égalité ne fonctionne pas pour les cellules" << endl;
+    }
+    if (!(c1 == c3)) {
+        cout << "[OK] l'opérateur d'égalité fonctionne pour deux cellules qui n'ont pas le même résultat" << endl;
+    } else {
+        cout << "[FAILURE] l'opérateur d'égalité ne fonctionne pas pour deux cellules qui n'ont pas le même résultat" << endl;
+    }
 }
 
 int main() {
-    // On fait appel aux trois méthodes, si tout est good --> OK
     testConstructeur();
     testConstructeurP();
     testSetEtat();
-    cout << "Le test unitaire est positif" << endl;
+    testOperatorEgal();
+    cout << "Le test unitaire fonctionne" << endl;
     return 0;
 }
